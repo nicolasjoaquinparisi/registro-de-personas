281bba855b3176f597ec84a6ae119768fabd90a6
Nicolás Joaquín Parisi <nicojp.1808@gmail.com>
2022-01-02 11:04:07 -0300

    New Jobs list UI

--- client/src/components/CardJob.jsx
+++ client/src/components/CardJob.jsx
@@ -26,16 +26,19 @@ const CardJob = ({job}) => {
     }
 
     return (
-        <div className="card shadow bg-light p-3 m-3 w-100 rounded">
-            <h3 className="text-center">{name}</h3>
-            <p className="p-2 text-secondary">{description}</p>
-
-            <div className="d-flex justify-content-end">
+        <div className="card shadow bg-light rounded">
+            <header className="bg-secondary p-3">
+                <h4 className="text-light text-center">{name}</h4>
+            </header>
+            <main className="h-100">
+                <p className="p-3 text-secondary">{description}</p>
+            </main>
+            <footer className="p-3 d-flex justify-content-end">
                 <Link to={{pathname: `/edit-job/${id}`}} >
                     <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="edit" style={{width:20}} className="me-2 edit svg-inline--fa fa-edit fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path></svg>
                 </Link>
                 <svg aria-hidden="true" onClick={handleClickDelete} focusable="false" data-prefix="far" data-icon="trash-can" style={{width:16}} className="delete svg-inline--fa fa-trash-can" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 80h-82.38l-34-56.75C306.1 8.827 291.4 0 274.6 0H173.4C156.6 0 141 8.827 132.4 23.25L98.38 80H16C7.125 80 0 87.13 0 96v16C0 120.9 7.125 128 16 128H32v320c0 35.35 28.65 64 64 64h256c35.35 0 64-28.65 64-64V128h16C440.9 128 448 120.9 448 112V96C448 87.13 440.9 80 432 80zM171.9 50.88C172.9 49.13 174.9 48 177 48h94c2.125 0 4.125 1.125 5.125 2.875L293.6 80H154.4L171.9 50.88zM352 464H96c-8.837 0-16-7.163-16-16V128h288v320C368 456.8 360.8 464 352 464zM224 416c8.844 0 16-7.156 16-16V192c0-8.844-7.156-16-16-16S208 183.2 208 192v208C208 408.8 215.2 416 224 416zM144 416C152.8 416 160 408.8 160 400V192c0-8.844-7.156-16-16-16S128 183.2 128 192v208C128 408.8 135.2 416 144 416zM304 416c8.844 0 16-7.156 16-16V192c0-8.844-7.156-16-16-16S288 183.2 288 192v208C288 408.8 295.2 416 304 416z"></path></svg>
-            </div>
+            </footer>
         </div>
     );
 }
--- client/src/routes/Jobs.js
+++ client/src/routes/Jobs.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import { Link } from "react-router-dom";
 import CardJob from '../components/CardJob';
 import { JobsContext } from '../context/JobsContext';
@@ -14,24 +14,22 @@ const Jobs = () => {
             <Link className="bg-transparent link-button border-0 fs-4 p-0 " to="/new-job">Nuevo empleo</Link>
         </div>
 
-        <main className="mt-5 d-flex justify-content-center table-responsive-sm">
+        <main className="mt-5 grid-jobs">
         {
-            Object.keys(jobs).length > 0 ?
-                (
-                    <div>
-                        {
-                            Object.keys(jobs).length > 0 ?
-                                jobs.map(job => (
-                                <CardJob
-                                    key={job.id}
-                                    job={job}
-                                />
-                                ))
-                            :
-                                null
-                        }
-                    </div>
-                )
+            (Object.keys(jobs).length > 0) ?
+            <Fragment>
+                    {
+                        Object.keys(jobs).length > 0 ?
+                            jobs.map(job => (
+                            <CardJob
+                                key={job.id}
+                                job={job}
+                            />
+                            ))
+                        :
+                            null
+                    }
+            </Fragment>
             :
                 <p className="fs-5 text-secondary">Por el momento no hay empleos dados de alta.</p>
         }
